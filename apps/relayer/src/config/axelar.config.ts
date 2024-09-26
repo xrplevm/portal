5d3fb7f2164837607e05fea5a53cd38d1270a88a
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-09-26 16:25:59 +0200

    refactor(relayer): replace secrets for process.env

--- apps/relayer/src/config/axelar.config.ts
+++ apps/relayer/src/config/axelar.config.ts
@@ -15,23 +15,22 @@ interface AxelarConfig {
 
 /**
  * Builds the logger configuration.
- * @param secrets The AWS secrets.
  * @returns The logger configuration.
  */
-export default (secrets: Record<any, any>): AxelarConfig => {
+export default (): AxelarConfig => {
     return buildConfig<AxelarConfig>({
-        rpcUrl: secrets.AXELAR_RPC_URL || "http://devnet-amplifier.axelar.dev:26657",
-        chainId: secrets.AXELAR_CHAIN_ID || "devnet-amplifier",
-        externalRelayWaitTime: Number(secrets.AXELAR_EXTERNAL_RELAY_WAIT_TIME) || 15_000,
-        verifyWaitTime: Number(secrets.AXELAR_VERIFY_WAIT_TIME) || 15_000,
-        proveWaitTime: Number(secrets.AXELAR_PROVE_WAIT_TIME) || 15_000,
-        privateKey: secrets.AXELAR_PRIVATE_KEY || "5b726ed6e1d4fdeec6ec7526d71c96218f6ebe4d7b10928732c49a242dd6bea9",
-        externalRelayedChains: secrets.AXELAR_EXTERNAL_RELAYED_CHAINS || ["avalanche-fuji"],
-        chainWebsockets: secrets.AXELAR_CHAIN_WEBSOCKETS || {
+        rpcUrl: process.env.AXELAR_RPC_URL || "http://devnet-amplifier.axelar.dev:26657",
+        chainId: process.env.AXELAR_CHAIN_ID || "devnet-amplifier",
+        externalRelayWaitTime: Number(process.env.AXELAR_EXTERNAL_RELAY_WAIT_TIME) || 15_000,
+        verifyWaitTime: Number(process.env.AXELAR_VERIFY_WAIT_TIME) || 15_000,
+        proveWaitTime: Number(process.env.AXELAR_PROVE_WAIT_TIME) || 15_000,
+        privateKey: process.env.AXELAR_PRIVATE_KEY || "5b726ed6e1d4fdeec6ec7526d71c96218f6ebe4d7b10928732c49a242dd6bea9",
+        externalRelayedChains: ["avalanche-fuji"],
+        chainWebsockets: {
             "avalanche-fuji": "wss://api.avax-test.network/ext/bc/C/ws",
             "xrpl-evm-sidechain": "ws://168.119.63.112:8546",
         },
-        supportedChains: secrets.AXELAR_SUPPORTED_CHAINS || ["avalanche-fuji"],
-        itsGasLimit: Number(secrets.AXELAR_ITS_GAS_LIMIT) || 8000000,
+        supportedChains: ["avalanche-fuji"],
+        itsGasLimit: Number(process.env.AXELAR_ITS_GAS_LIMIT) || 8000000,
     });
 };
--- apps/relayer/src/config/index.ts
+++ apps/relayer/src/config/index.ts
@@ -1,19 +1,15 @@
 import serverConfig from "./server.config";
 import axelarConfig from "./axelar.config";
 import loggerConfig from "./logger.config";
-import { loadAwsSecrets } from "@backend/config";
-import awsS3Config from "./aws-s3.config";
 
 /**
  * Builds the application configuration.
  * @returns The application configuration.
  */
 export default async (): Promise<any> => {
-    const secrets = await loadAwsSecrets(process.env.AWS_REGION || "", process.env.AWS_SECRET_ID || "");
     return {
         server: serverConfig(),
-        axelar: axelarConfig(secrets),
+        axelar: axelarConfig(),
         logger: loggerConfig(),
-        awsS3: awsS3Config(secrets),
     };
 };

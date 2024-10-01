b99a98e81627121a3d762605feb2450d426d20a0
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-10-01 15:55:06 +0200

    feat(frontend/misc): add `DATE_TIME` date strategy

--- packages/frontend/misc/src/ui/date/strategies/index.ts
+++ packages/frontend/misc/src/ui/date/strategies/index.ts
@@ -4,3 +4,4 @@ export * from "./format-minimal-date";
 export * from "./format-month";
 export * from "./format-month-year";
 export * from "./format-short-date";
+export * from "./format-date-time";

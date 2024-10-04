17d08fc6570b272b7d32c9dd5b6eca29217f5ae1
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-10-04 13:23:39 +0200

    feat(frontend/activity): add activity controller testing

--- packages/frontend/activity/test/tests/domain/controllers/activity.controller.spec.ts
+++ packages/frontend/activity/test/tests/domain/controllers/activity.controller.spec.ts
@@ -5,6 +5,12 @@ import { ActivityServiceMock } from "../../../mocks/domain/interfaces/activity.s
 describe("ActivityController", () => {
     let activityController: ActivityController;
 
+    const page = 1;
+    const pageSize = 10;
+    const sourceChainAddress = "0x123";
+    const destinationChainAddress = "0x456";
+    const senderAddress = "0x789";
+
     const activityServiceMock = new ActivityServiceMock();
 
     beforeEach(async () => {
@@ -15,9 +21,22 @@ describe("ActivityController", () => {
 
     describe("getPaginatedTransfers", () => {
         it("should return the paginated transfers", async () => {
-            const transfers = await activityController.getPaginatedTransfers();
+            const transfers = await activityController.getPaginatedTransfers(
+                page,
+                pageSize,
+                sourceChainAddress,
+                destinationChainAddress,
+                senderAddress,
+            );
 
-            expect(transfers).toEqual(activityServiceMock.getPaginatedTransfers());
+            expect(transfers).toHaveLength(2);
+            expect(activityServiceMock.getPaginatedTransfers).toHaveBeenCalledWith(
+                page,
+                pageSize,
+                sourceChainAddress,
+                destinationChainAddress,
+                senderAddress,
+            );
         });
     });
 });
